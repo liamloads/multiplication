56838461e2870260032bd24696c3db9d4e2afbaf
Will Parsons <twexxor@gmail.com>
2022-05-09 17:22:52 -0700

    Update twexxor-multiplication-calculator.js

--- twexxor-multiplication-calculator.js
+++ twexxor-multiplication-calculator.js
@@ -1,6 +1,6 @@
 const _16_0 = {'e': 1, 'E': 1, 'NaN': 1};
 const _16_1 = require('twexxor-addition-calculator');
-const _16_2 = require('twexxor-highest-detector');
+const _16_2 = require('twexxor-lowest-detector');
 const _16_3 = require('twexxor-string-repeater');
 
 const twexxorMultiplicationCalculator = function(_16_4, _16_5) {
@@ -8,5 +8,7 @@ const twexxorMultiplicationCalculator = function(_16_4, _16_5) {
 		_16_1(_16_4, '0'),
 		_16_1(_16_5, '0')
 	];
+	_16_5 = _16_2(_16_4);
+	let _16_6 = (_16_5 === _16_4[0]) - 0;
 	// todo
 };
