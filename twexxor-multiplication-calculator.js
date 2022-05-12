ca009bfe84a909fac56e543261b670c0528c9394
Will Parsons <twexxor@gmail.com>
2022-05-12 16:26:49 -0700

    Update twexxor-multiplication-calculator.js

--- twexxor-multiplication-calculator.js
+++ twexxor-multiplication-calculator.js
@@ -186,6 +186,11 @@ const twexxorMultiplicationCalculator = function(_16_5, _16_6) {
 		_16_6++;
 
 		while (_16_6 !== _16_11) {
+			if (_16_5[_16_11] === '.') {
+				_16_11++
+				continue;
+			}
+
 			_16_12 += _16_5[_16_11++];
 		}
 
